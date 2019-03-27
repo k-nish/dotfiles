a979b0c7d7f4a777bef88bdc7ea11a2943118fb7
k-nish <koh.oct.un@gmail.com>
2019-03-27 16:57:55 +0900

    [add] update vimrc in update.sh

--- update.sh
+++ update.sh
@@ -1,5 +1,6 @@
 cp ./nvim/init.vim ~/.config/nvim/
 cp ./nvim/dein.toml ~/.config/nvim/
+cp ./vim/.vimrc ~/
 cp ./flake8 ~/.config/
 cp ./tmux/.tmux.conf ~/.tmux.conf
 echo 'update config files of neovim, flake8, tmux'
